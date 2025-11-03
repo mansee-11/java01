46afa45fe84d27f64e6b31ba92d215e1e45d5729
Mansee <silochanaasati30@gmail.com>
2025-11-03 20:26:20 +0530

    lect01

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Setup Successful");
+        System.out.println("welocome to java");
+        int a=1123;     //4 byte storage
+        byte b=50;      //1 byte storage
+        short c=100;    //2 byte storage
+        char d='m';     //2 byte storage
+        float e= 3.14f; //specefy float by writing f confuse over double
+
+        //to check odd even
+        if(a%2==0)
+        {
+            System.out.println("number is even");
+        }
+        else {
+            System.out.println("number is odd");
+        }
     }
 }
