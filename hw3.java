e098adb37d86e2da0cad81670a28cfaf5ffc8dab
Mansee <silochanaasati30@gmail.com>
2025-11-13 18:49:08 +0530

    self solving

--- hw3.java
+++ hw3.java
@@ -1,6 +1,6 @@
 //to save 3student data using  class
 
-class student {
+/*class student {
     String name;
     int rollno,marks;
     void disp() {
@@ -9,8 +9,33 @@ class student {
         System.out.println("marks = " + marks);
     }
 }
+class rectangle{
+    int length,bredth;
+    void area()
+    {
+        System.out.println("area of the rectangle is "+(length*bredth));
+    }
+    void perameter()
+    {
+        System.out.println("parameter of rectangle is "+(2*(length+bredth)));
+    }
+}*/
+class circle{
+    double  radius;
+    void area()
+    {
+        System.out.println("area of circle is "+(3.14)*radius*radius);
+    }
+    void circumference()
+    {
+        System.out.println("circumference of circle is "+(3.14)*2*radius);
+    }
+}
 public class hw3 {
     public static void main(String[] agrs) {
+        /*
+        //to make object of student class
+
         student s1=new student();
         student s2=new student();
         student s3=new student();
@@ -29,5 +54,34 @@ public class hw3 {
         s1.disp();
         s2.disp();
         s3.disp();
+
+        //to make object of rectangle class
+        rectangle r1=new rectangle();
+        rectangle r2=new rectangle();
+
+        r1.bredth=20;
+        r1.length=45;
+
+        r2.bredth=11;
+        r2.length=23;
+
+        r1.area();
+        r1.perameter();
+
+        r2.area();
+        r2.perameter();
+        */
+        //circle class
+        circle c1=new circle();
+        circle c2=new circle();
+
+        c1.radius=23;
+        c2.radius=11;
+
+        c1.area();
+        c1.circumference();
+
+        c2.area();
+        c2.circumference();
     }
 }
