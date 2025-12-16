f636c33c68992052c61150bfc09e844c5b32e5b9
Mansee <silochanaasati30@gmail.com>
2025-12-16 22:50:39 +0530

    que practice array

---
+++ lect23.java
@@ -0,0 +1,24 @@
+//to get majority element from array(element arriving n/2+1 times)
+public class lect23 {
+    public static int majority(int[] nums)
+    {
+        int count=0;
+        int ans=0;
+        for(int i:nums) {
+            if (count == 0) {
+                ans = i;
+            }
+            if (i == ans) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args)
+    {
+        int arr[]={2,2,3,2,5};
+        System.out.println(majority(arr));
+    }
+}
